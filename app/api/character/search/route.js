7d4bf187207a694cf0c728e5065497590105b09e
minhyeonhong <mhh2029@gmail.com>
2024-01-23 09:35:05 +0900

    ENH :  cors 미들웨어

--- app/api/character/search/route.js
+++ app/api/character/search/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextRequest,NextResponse } from 'next/server';
 import { LowDB } from '@/common/jsonDB';
 import { get_character_info } from '@/services/CharacterService';
 import { today } from '@/common/date';
@@ -30,31 +30,6 @@ export async function POST(req) {
 
     console.log("result :", result);
   }
-
-
-
-
-
-
-  // db.data.characters.push('hello world')
-  // await db.write();
-
-
-  // let existingData = [];
-  // try {
-  //   existingData = await readJson(filePath);
-  // } catch (error) {
-  //   // 파일이 존재하지 않을 경우 무시하거나 적절한 오류 처리를 수행할 수 있습니다.
-  //   console.error('Error reading existing data:', error);
-  // }
-  // // 새로운 데이터 추가
-  // existingData.push(request);
-
-  // // 합쳐진 데이터 저장
-  // await writeJson(filePath, existingData);
-
-  // // 저장 후 데이터 다시 읽어오기 (옵션)
-  // const updatedData = await readJson(filePath);
 
   return NextResponse.json({
     message: 'Data written successfully',
