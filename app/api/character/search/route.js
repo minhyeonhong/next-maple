eb0b57f07d7e06900cf176c76b52ac2a41bc4923
minhyeonhong <mhh2029@gmail.com>
2024-01-24 17:04:15 +0900

    TST : lodash를 사용해서 lowdb관리 하는 방법으로 수정중

--- app/api/character/search/route.js
+++ app/api/character/search/route.js
@@ -1,71 +1,88 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { LowDB } from '@/common/jsonDB';
+//import { LowDB } from '@/common/jsonDB';
 import { api_maple_character_info } from '@/services/CharacterService';
 import { today, isToday } from '@/common/date';
+
+import { JSONFile } from 'lowdb/node';
+import { Low } from 'lowdb';
+import path from 'path';
+import lodash from 'lodash'
+
+class LowWithLodash extends Low {
+  constructor(adapter) {
+    super(adapter, { [Object.keys(adapter)[0]]: [] });
+    this.chain = lodash.chain(this).get('data');
+  }
+}
 
 export async function POST(req) {
 
   const request = await req.json();
-
+  const dbPath = path.resolve('db');
   const lowdb = new LowDB('characters', { characters: [] });
 
-  const table = await lowdb.table;
+  const adapter = new JSONFile(`${dbPath}/characters.json`, { characters: [] });
 
-  let resultCharacter = {};
+  const db = new LowWithLodash(adapter);
+  await db.read();
 
-  if (table.length > 0) {
-    const dbCharacter = await lowdb.findLowField('character_name', '이상한');
+  console.log('lowdb @@', db.data);
 
-    if (dbCharacter) {
-       //db의 데이터가 오늘날짜가 아니면
-    if (!isToday(dbCharacter.today)) {
-      //메이플 api타고 캐릭정보 가져오기
-      const character = await api_maple_character_info(request.character_name);
-      //여기서 부터 짜야해!
-      }
-    } else {
-      //메이플 api타고 캐릭정보 가져오기
-      const character = await api_maple_character_info(request.character_name);
-      const newCharacter = await lowdb.createField(character);
-      
-      if (!newCharacter) {
-        return NextResponse.json({
-          success: false,
-          message: '캐릭터 가져오기 실패',
-        },
-          {
-            status: 400,
-          });
-      }
+  // const table = await lowdb.table;
 
-      resultCharacter = character;
-    }
+  // let resultCharacter = {};
+
+  // if (table.length > 0) {
+  //   const dbCharacter = await lowdb.findLowField('character_name', '이상한');
+
+  //   if (dbCharacter) {
+  //      //db의 데이터가 오늘날짜가 아니면
+  //   if (!isToday(dbCharacter.today)) {
+  //     //메이플 api타고 캐릭정보 가져오기
+  //     const character = await api_maple_character_info(request.character_name);
+  //     //여기서 부터 짜야해!
+  //     }
+  //   } else {
+  //     //메이플 api타고 캐릭정보 가져오기
+  //     const character = await api_maple_character_info(request.character_name);
+  //     const newCharacter = await lowdb.createField(character);
+
+  //     if (!newCharacter) {
+  //       return NextResponse.json({
+  //         success: false,
+  //         message: '캐릭터 가져오기 실패',
+  //       },
+  //         {
+  //           status: 400,
+  //         });
+  //     }
+
+  //     resultCharacter = character;
+  //   }
 
 
-    
-    console.log('db에서 불러온 캐릭터 정보', dbCharacter);
 
-  } else {
-    console.log("여긴 왔지? ");
+  //   console.log('db에서 불러온 캐릭터 정보', dbCharacter);
 
-    //메이플 api타고 캐릭정보 가져오기
-    const character = await api_maple_character_info(request.character_name);
+  // } else {
+  //   console.log("여긴 왔지? ");
 
-    const result = await lowdb.createField('characters', {
-      today,
-      character_name: request.character_name,
-      ocid: 123,
-    });
+  //   //메이플 api타고 캐릭정보 가져오기
+  //   const character = await api_maple_character_info(request.character_name);
 
-    console.log("result :", result);
-  }
+  //   const result = await lowdb.createField('characters', {
+  //     today,
+  //     character_name: request.character_name,
+  //     ocid: 123,
+  //   });
+
+  //   console.log("result :", result);
+  // }
 
   return NextResponse.json({
     success: true,
     message: '캐릭터 가져오기 성공',
-    character : resultCharacter,
-
-    //updatedData: updatedData // 저장 후의 데이터를 확인하려면 필요합니다 (실제 프로덕션 코드에서는 필요 없을 수 있음).
+    //character : resultCharacter,
   });
 }
 
--- common/jsonDB.js
+++ common/jsonDB.js
@@ -1,59 +1,72 @@
-import { JSONFilePreset } from 'lowdb/node';
+import { JSONFilePreset, JSONFile } from 'lowdb/node';
+import { Low } from 'lowdb';
 import path from 'path';
 import lodash from 'lodash'
 
 const dbPath = path.resolve('db');
 
+class LowWithLodash extends Low {
+    constructor(adapter) {
+        super(adapter, { [Object.keys(adapter)[0]]: [] });
+        this.chain = lodash.chain(this).get('data');
+    }
+}
+
 class LowDB {
     constructor(jsonName, defaultData) {
-        this.db = this.initializeDB(jsonName, defaultData);
-        this.table = this.initializeTable(defaultData);
+        const adapter = new JSONFile(`${dbPath}/${jsonName}.json`, defaultData);
+        this.db = new LowWithLodash(adapter);
     }
 
-    async initializeDB(jsonName, defaultData) {
-        return await JSONFilePreset(`${dbPath}/${jsonName}.json`, defaultData);
-    }
+    // constructor(jsonName, defaultData) {
+    //     this.db = this.initializeDB(jsonName, defaultData);
+    //     this.table = this.initializeTable(defaultData);
+    // }
 
-    async initializeTable(defaultData) {
-        return (await this.db).data[Object.keys(defaultData)[0]];
-    }
+    // async initializeDB(jsonName, defaultData) {
+    //     return await JSONFilePreset(`${dbPath}/${jsonName}.json`, defaultData);
+    // }
 
-    async findLowField(findField, findValue) {
-        return (await this.table).find((item) => item[findField] === findValue);
-    }
+    // async initializeTable(defaultData) {
+    //     return (await this.db).data[Object.keys(defaultData)[0]];
+    // }
 
-    async createField(item) {
-        try {
-            (await this.table).push(item);
-            (await this.db).write();
-            return true;
-        } catch (error) {
-            console.log('createField error :', error);
-            return false;
-        }
-    }
+    // async findLowField(findField, findValue) {
+    //     return (await this.table).find((item) => item[findField] === findValue);
+    // }
 
-    async updateField(item) {
-        try {
-            (await this.table).push(item);
-            (await this.db).write();
-            return true;
-        } catch (error) {
-            console.log('createField error :', error);
-            return false;
-        }
-    }
+    // async createField(item) {
+    //     try {
+    //         (await this.table).push(item);
+    //         (await this.db).write();
+    //         return true;
+    //     } catch (error) {
+    //         console.log('createField error :', error);
+    //         return false;
+    //     }
+    // }
 
-    async deleteField(item) {
-        try {
-            (await this.table).push(item);
-            (await this.db).write();
-            return true;
-        } catch (error) {
-            console.log('createField error :', error);
-            return false;
-        }
-    }
+    // async updateField(item) {
+    //     try {
+    //         (await this.table).push(item);
+    //         (await this.db).write();
+    //         return true;
+    //     } catch (error) {
+    //         console.log('createField error :', error);
+    //         return false;
+    //     }
+    // }
+
+    // async deleteField(item) {
+    //     try {
+    //         (await this.table).push(item);
+    //         (await this.db).write();
+    //         return true;
+    //     } catch (error) {
+    //         console.log('createField error :', error);
+    //         return false;
+    //     }
+    // }
 }
 
 
