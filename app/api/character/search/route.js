59076884f8984f1e969d67725617aee5eceb7aea
minhyeonhong <mhh2029@gmail.com>
2024-01-26 09:25:23 +0900

    ENH : 캐릭정보 가져오는 api 완성

--- app/api/character/search/route.js
+++ app/api/character/search/route.js
@@ -1,66 +1,44 @@
 import { NextResponse } from 'next/server';
 import { LowDB } from '@/common/LowDB';
 import { api_maple_character_info } from '@/services/CharacterService';
-import { today, isToday } from '@/common/date';
+import { isToday } from '@/common/date';
 
 export async function POST(req) {
-
   const request = await req.json();
   const lowdb = new LowDB('characters', 'characters');
+  let dbCharacter = null;
 
-  //await lowdb.updateField('ocid',1234,{ today: '2024-01-23', character_name: '푸하아앙잉', ocid: 321 });
-  console.log('================');
+  // db에 캐릭정보 가져오기
+  dbCharacter = await lowdb.findLowField('character_name', request.character_name);
 
-  const c = await lowdb.findLowField('character_name', '푸하아앙잉');
-  //const c = await lowdb.updateField('character_name', '푸하아앙잉', { today: '2024-01-23', character_name: '푸하아앙잉', ocid: 321 });
-  //const c = await lowdb.deleteField('character_name', '푸하아앙잉');
-  console.log((await lowdb.table));
+  // 데이터가 없거나 오늘날짜가 아닌 경우 새로운 데이터를 가져옴
+  if (!dbCharacter || !isToday(dbCharacter.data.today)) {
+    const freshData = await api_maple_character_info(request.character_name);
+    const isUpdatedOrCreated = dbCharacter 
+      ? await lowdb.updateField('character_name', request.character_name, freshData)
+      : await lowdb.createField(freshData);
 
-
-  if ((await lowdb.table).length > 0) {
-    console.log("응 있어");
-    const dbCharacter = await lowdb.findLowField('character_name', request.character_name);
-
-    // if (dbCharacter) {
-    //    //db의 데이터가 오늘날짜가 아니면
-    // if (!isToday(dbCharacter.today)) {
-    //   //메이플 api타고 캐릭정보 가져오기
-    //   const character = await api_maple_character_info(request.character_name);
-    //   //여기서 부터 짜야해!
-    //   }
-    // } else {
-    //   //메이플 api타고 캐릭정보 가져오기
-    //   const character = await api_maple_character_info(request.character_name);
-    //   const newCharacter = await lowdb.createField(character);
-
-    //   if (!newCharacter) {
-    //     return NextResponse.json({
-    //       success: false,
-    //       message: '캐릭터 가져오기 실패',
-    //     },
-    //       {
-    //         status: 400,
-    //       });
-    //   }
-
-    //   resultCharacter = character;
-    // }
-
-  } else {
-
-
-    //메이플 api타고 캐릭정보 가져오기
-    const character = await api_maple_character_info(request.character_name);
-    const result = await lowdb.createField(character);
-
-    console.log("result :", result);
+    if(isUpdatedOrCreated){
+      dbCharacter = freshData;
+    } else {
+      return fail();
+    }
   }
-
 
   return NextResponse.json({
     success: true,
     message: '캐릭터 가져오기 성공',
-    //character : resultCharacter,
+    character: dbCharacter,
   });
+}
+
+const fail = () => {
+  return NextResponse.json({
+    success: false,
+    message: '캐릭터 가져오기 실패',
+  },
+    {
+      status: 400,
+    });
 }
 
