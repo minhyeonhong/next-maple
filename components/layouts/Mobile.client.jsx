09eb8b577ebf40c663f4015acff3d04db22fa49e
minhyeonhong <mhh2029@gmail.com>
2024-02-01 10:16:08 +0900

    ENH : 컴포넌트에서 불필요한 vh인자 제거

--- components/layouts/Mobile.client.jsx
+++ components/layouts/Mobile.client.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-const Mobile = ({ vh, children }) => {
+const Mobile = ({ children }) => {
     return (
         <div>
             { children }
--- components/layouts/Tablet.client.jsx
+++ components/layouts/Tablet.client.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-const Tablet = ({ vh, children }) => {
+const Tablet = ({ children }) => {
     return (
         <div>
             { children }
