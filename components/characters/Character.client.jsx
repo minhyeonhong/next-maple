9edcea3848f8d204fe8f370e6bc4d88e93ff21ee
minhyeonhong <mhh2029@gmail.com>
2024-02-01 15:33:16 +0900

    ENH : 레이아웃 수정중

--- components/characters/Character.client.jsx
+++ components/characters/Character.client.jsx
@@ -32,14 +32,25 @@ const Character = () => {
 
   return (
     <div className="h-[100%] grid grid-rows-4">
-      <div className="row-span-1 flex justify-center bg-red-100 ">
+      <div className="row-span-1 flex flex-col justify-center items-center bg-red-100 ">
         로고
       </div>
-      <div className="row-span-1 bg-blue-100">
+      <div className="row-span-1 flex flex-col items-center bg-blue-100">
         <input type='text' name='character_name' />
+
+        <div class="relative mt-2 rounded-md shadow-sm">
+          <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span class="text-gray-500 sm:text-sm">$</span>
+          </div>
+          <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <div>dd</div>
+          </div>
+        </div>
+
         <button onClick={searchCharacter}>캐릭터 정보 가져오기</button><br />
       </div>
-      <div className="row-span-2 bg-green-100">
+      <div className="row-span-2 flex flex-col items-center bg-green-100">
         캐릭 정보
       </div>
     </div>
