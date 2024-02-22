2739c6b41537e3088868840529902f27914eaab7
Minhyeonhong <mhh2029@gmail.com>
2024-02-22 22:43:30 +0900

    ENH : globals.css에서 font-family적용 되도록 수정

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-['LINESeedKR-Rg']">
+      <body>
         <ReactQueryProviders>
           <Layout>{children}</Layout>
         </ReactQueryProviders>
